8c3f6316b2194e757d4a87f01aad7290012ed665
Robert <rquinones93@yahoo.com>
2018-09-23 01:40:41 -0700

    - Updated Procfile to include proper script Heroku runs
    - Updated app.js with routers / adding node modules to app
    
    - Updated package.json with more modules
    
    - Added server in bin/www
    
    - Added about and index page routes
    
    - Added partials and ejs layout files for front end dev

--- app.js
+++ app.js
@@ -1,21 +1,89 @@
 const express = require('express');
 const path = require('path');
+// Commenting Out until useful
+const expressLayouts = require('express-ejs-layouts');
+const expressValidator = require('express-validator');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+// const passport = require('passport');
+// const session = require('express-session');
 const PORT = process.env.PORT || 5000;
 
+// Make use of environment variables defined in .env
+if ( process.env.NODE_ENV === 'development' ||
+     process.env.NODE_ENV === 'production') {
+  require('dotenv').config();
+}
+
+// Declare Express App
 const app = express();
 
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/times', (req, res) => {
-  let result = '';
-  const times = process.env.TIMES || 5;
-  for (i = 0; i < times; i++) {
-    result += i + ' ';
-  }
-  res.send(result);
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(expressLayouts);
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routers 
+const index = require('./routes/index');
+const about = require('./routes/about');
+
+// Middleware for routes
+app.use('/', index);
+app.use('/about', about);
+
+// Passport Initialize
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// Express Validator - Taken from Middleware Options on Github
+app.use(
+  expressValidator({
+    errorFormatter: function (param, msg, value) {
+      let namespace = param.split('.'),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
+
+// catch 404 and forward to error handler
+app.use(function (req, response, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handler
+app.use(function (err, req, response, next) {
+  // set locals, only providing error in development
+  response.locals.message = err.message;
+  response.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  response.status(err.status || 500);
+  response.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+// Serve the index page
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (request, response, next) => {
+  response.render('pages/index');
+});
+
+module.exports = router;
