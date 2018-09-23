d591e37c1ab69e0f55e79acf5f3bef3d3c765724
Robert <rquinones93@yahoo.com>
2018-09-22 22:48:55 -0700

    Provisioned database in tutorial

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
-express()
+const app = express();
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.get('/times', (req, res) => {
+  let result = '';
+  const times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++) {
+    result += i + ' ';
+  }
+  res.send(result);
+});
+
+app.get('/db', (req, res) => {
+  try {
+    const client = pool.connect();
+    const result = client.query('SELECT * FROM test_table');
+    const results = {
+      'results': (result) ? result.rows : null
+    };
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
