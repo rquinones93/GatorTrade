ae0a981e4f603b1d3d13447b8cac75e18549c430
Robert <rquinones93@yahoo.com>
2018-09-24 19:06:19 -0700

    Renamed dhruv's images and name in code
    Updated about.ejs with links to about me pages
    
    added robert.ejs in developer_profile for template

--- routes/about.js
+++ routes/about.js
@@ -5,4 +5,9 @@ router.get('/', (request, response, next) => {
   response.render('pages/about');
 });
 
+router.get('/:name', (request, response, next) => {
+  let developer_name = request.params.name;
+  response.render('pages/dev_profile/' + developer_name);
+});
+
 module.exports = router;
