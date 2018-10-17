9e70f68a0eab9cad623907bb0651fb754ac8a153
Robert <rquinones93@yahoo.com>
2018-10-17 14:18:19 -0700

    Created Categories Seeder File

---
+++ seeders/20181017211310-categories.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // Adds ALL categories to DB
+    return queryInterface.bulkInsert('categories', [{
+      title: 'Text Books',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      title: 'Electronics',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      title: 'Home Appliances',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      title: 'Student Services',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    // Delete Table
+    return queryInterface.bulkDelete('categories', null, {});
+  }
+};
