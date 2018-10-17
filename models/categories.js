7277dcfb1cac5b716e359bb5b87ebc3399f27b59
Robert <rquinones93@yahoo.com>
2018-10-17 00:43:04 -0700

    Reworked category model/migration file

--- models/categories.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Categories = sequelize.define('categories', {
-    title: DataTypes.STRING
-  }, {});
-  
-  Categories.associate = function(models) {};
-  return Categories;
-};
