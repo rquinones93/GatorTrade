de100ceb4a33f70e8723812108933b500dab22cf
Robert <rquinones93@yahoo.com>
2018-10-17 14:42:06 -0700

    Created images seeder file

---
+++ seeders/20181017213636-images.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('images', [{
+      itemId: 1,
+      imageLink: 'https://res.cloudinary.com/hx8ztvtac/image/upload/c_scale,w_250/v1539125823/samples/ecommerce/shoes.png',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      itemId: 2,
+      imageLink: 'https://res.cloudinary.com/hx8ztvtac/image/upload/c_scale,w_250/v1539125830/samples/ecommerce/leather-bag-gray.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      itemId: 3,
+      imageLink: 'https://res.cloudinary.com/hx8ztvtac/image/upload/c_scale,w_250/v1539811936/samples/ecommerce/books.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('images', null, {});
+  }
+};
