a2d533b5a4947dc87ff350a0539b0df75b7aa6a4
Robert <rquinones93@yahoo.com>
2018-10-17 15:04:38 -0700

    Created messages seeder file

--- seeders/20181017213636-images.js
+++ seeders/20181017213636-images.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
+    // Add images for the 3 seeded items
     return queryInterface.bulkInsert('images', [{
       itemId: 1,
       imageLink: 'https://res.cloudinary.com/hx8ztvtac/image/upload/c_scale,w_250/v1539125823/samples/ecommerce/shoes.png',
