0e55ee381898a2c06f22542ace0b7d50d15e86ab
Janda95 (Mars) <j.rettinghouse@gmail.com>
2018-10-29 20:29:01 -0700

    post query added, need to change database itemId to itemid

--- routes/post.js
+++ routes/post.js
@@ -1,19 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
+
+const db = require('../database/index');
+
+
 router.get('/', (request, response, next) => {
     response.render('pages/post',{
         title: "Post"
     });
 });
 
-router.get('/:post', (request, response, next) => {
+router.get('/:post_id', (request, response, next) => {
     let localpostId = request.params.post;
 
-    response.render('pages/post', {
-      title: "Post " + localpostId,
-      postID: localpostId
+    let searchInput = 'SELECT items.item_id FROM items WHERE items.item_id = $1';
+
+
+    db.any(searchInput, [localpostId])
+    .then( items => {
+         console.log(items);
+        response.render('pages/post', {
+            title: "Post " + localpostId,
+            items: items,
+            currentCategory: currentCategory,
+            post_id: localpostId
+        });
+    }).catch( err =>{
+        console.log(err);
     });
+    
+
+
+
 });
 
 module.exports = router;
