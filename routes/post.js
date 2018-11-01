bb32d160286795df04e8b23002e4fbbebce3c4ef
Janda95 (Mars) <j.rettinghouse@gmail.com>
2018-10-31 17:35:20 -0700

    oneOrNone

--- routes/post.js
+++ routes/post.js
@@ -7,24 +7,19 @@ const router = express.Router();
 const db = require('../database/connection');
 
 
-router.get('/', (request, response, next) => {
-    response.render('pages/post',{
-        title: "Post"
-    });
-});
-
 router.get('/:post_id', (request, response, next) => {
     let localpostId = request.params.post_id;
 
     let searchInput = 'SELECT items.item_id FROM items WHERE items.item_id = $1';
 
 
-    db.any(searchInput, [localpostId])
+    db.oneOrNone(searchInput, [localpostId])
     .then( item => {
          console.log(item);
         response.render('pages/post', {
             title: "Post " + localpostId,
-            item: item
+            item: item,
+            post_id: localpostId
         });
     }).catch( err =>{
         console.log(err);
