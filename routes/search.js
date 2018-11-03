c8a7d533d9b18413db92186f8fa8252f87309562
Robert <rquinones93@yahoo.com>
2018-11-03 08:56:44 -0700

    Simple way of searching from home page

--- routes/search.js
+++ routes/search.js
@@ -17,11 +17,18 @@ router.get('/', (request, response, next) => {
 });
 
 router.post('/', (request, response, next) => { //when hit search button
-  let searchInput = request.body.searchInput.toLowerCase();
-  let searchCategory = request.body.categories;
+  // let searchInput = (request.body.searchInput == undefined) ? "" : request.body.searchInput;
+  // let searchCategory = request.body.categories;
+  let searchInput = "";
+  let searchCategory = "";
+  if(request.body.searchInput) { 
+    searchInput = request.body.searchInput; 
+  }
 
   Search.search(searchInput, searchCategory)
     .then(items => {
+
+      console.log(items);
       response.render('pages/search', {
         title: "GatorTrade - Search",
         items: items,
