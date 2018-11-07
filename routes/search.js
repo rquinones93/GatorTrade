6d77b6c4b4af931b9ff55009d89a72e48bc4d847
Abigail Chin <chinabigail08@gmail.com>
2018-11-07 15:28:54 -0800

    changed to include casing (not all lower case) for search inputs

--- routes/search.js
+++ routes/search.js
@@ -20,11 +20,11 @@ router.post('/', (request, response, next) => {
   // Search Input can be undefined if accessing from index or
   // not input by user.
   let searchInput = (request.body.searchInput != undefined) ? 
-                     request.body.searchInput.toLowerCase() : "";
-
+                     request.body.searchInput : "";
+  
   let searchCategory = request.body.categories;
 
-  Search.search(searchInput, searchCategory)
+  Search.search(searchInput.toLowerCase(), searchCategory)
     .then(items => {
       response.render('pages/search', {
         title: "GatorTrade - Search",
