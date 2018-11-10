b2862297973b7c49bc7238b212159cec0ac40883
Jed Ahmadia <jahmadia@yahoo.com>
2018-11-09 19:01:51 -0800

    Added initial SingIN files

--- app.js
+++ app.js
@@ -40,12 +40,14 @@ const index = require('./routes/index');
 const about = require('./routes/about');
 const search = require('./routes/search');
 const post = require('./routes/post');
+const signin = require('./routes/signin');
 
 // Middleware for routes
 app.use('/', index);
 app.use('/about', about);
 app.use('/search', search);
 app.use('/post', post);
+app.use('/signin', signin);
 
 // Passport Initialize
 // app.use(passport.initialize());
