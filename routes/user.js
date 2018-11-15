e68f343bd930021554bc715191bfe97ba7a2663b
Janda95 (Mars) <j.rettinghouse@gmail.com>
2018-11-15 15:47:57 -0800

    messages show up in new messages(not specific to user)

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('../database/connection');
+
 router.get('/', (request, response, next) => {
-  response.render('pages/user', {
-    title: "GatorTrade - User"
-  });
+
+  let messageSearch = 'SELECT * FROM messages';
+  //let messageSearch = 'SELECT * FROM messages WHERE /*USER VALUE GOES HERE*/'; 
+
+  //let messages = db.query(messageSearch);
+  //let posts = db.query(postSearch);
+
+
+  db.query(messageSearch, [])
+    .then(messages => {
+      response.render('pages/user', {
+        title: "GatorTrade - User",
+        messages: messages
+      });
+    });
 });
 
 module.exports = router;
