b57dd5d1226b27eb9ac3389ff8368dc6a2d39809
Janda95 (Mars) <j.rettinghouse@gmail.com>
2018-11-15 16:09:21 -0800

    Double query for posts and messages, comments added in user.ejs

--- routes/user.js
+++ routes/user.js
@@ -4,21 +4,27 @@ const router = express.Router();
 const db = require('../database/connection');
 
 router.get('/', (request, response, next) => {
-
+  //Currently displays all messages
   let messageSearch = 'SELECT * FROM messages';
-  //let messageSearch = 'SELECT * FROM messages WHERE /*USER VALUE GOES HERE*/'; 
+  
+  //Todo: USER specific
+  //let messageSearch = 'SELECT * FROM messages WHERE __________';
 
-  //let messages = db.query(messageSearch);
-  //let posts = db.query(postSearch);
-
+  let postSearch = 'SELECT * FROM items WHERE seller_id = $1';
+  //User specific id placeholder
+  let tempSellerID = 1;
 
   db.query(messageSearch, [])
     .then(messages => {
-      response.render('pages/user', {
-        title: "GatorTrade - User",
-        messages: messages
-      });
-    });
+      db.query(postSearch, [tempSellerID])
+        .then(posts => {
+          response.render('pages/user', {
+            title: "GatorTrade - User",
+            messages: messages,
+            posts: posts
+          });
+      }); 
+  });
 });
 
 module.exports = router;
