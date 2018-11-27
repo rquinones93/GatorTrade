7a63d4135c3819d46c5aa445c740724842dc15e5
Robert <rquinones93@yahoo.com>
2018-11-27 14:11:57 -0800

    Added specific user_id to the user dashboard

--- routes/user.js
+++ routes/user.js
@@ -6,14 +6,14 @@ const db = require('../database/connection');
 router.get('/', (request, response, next) => {
   //Currently displays all messages
   let messageSearch = 'SELECT * FROM messages WHERE seller_id = $1';
-  let sellID = 1;
+  let sellID = request.user.user_id;
   
   //Todo: USER specific
   //let messageSearch = 'SELECT * FROM messages WHERE __________';
 
   let postSearch = 'SELECT * FROM items WHERE seller_id = $1';
   //User specific id placeholder
-  let tempSellerID = 1;
+  let tempSellerID = request.user.user_id;
 
   db.query(messageSearch, [sellID])
     .then(messages => {
