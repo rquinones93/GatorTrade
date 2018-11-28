ace500010c038fcc3e639242184611f63c63fcf3
Robert <rquinones93@yahoo.com>
2018-11-27 23:31:20 -0800

    pushing changes to see jed's branch

--- routes/signup.js
+++ routes/signup.js
@@ -43,34 +43,27 @@ router.get('/', (request, response, next) => {
   });
 });
 
-router.post('/', upload.single('image'), (request, response, next) => {
+router.post('/', (request, response, next) => {
+
   let formErrors = formValidation( request );
 
-  if( formErrors ) {
-    renderErrors(response, formErrors);
+  if( formErrors ) { 
+    renderErrors(response, formErrors); 
 
   } else {
+
     const { first_name, last_name, email, password } = request.body;
-    const image_path = request.file.path;
 
-    cloudinary.uploader.upload(image_path, (result) => {
-      const profile_picture = result.secure_url;
-      const public_id = result.public_id;
+    // Create a new user
+    User.create(first_name, last_name, email, password)
+      .then( errors => {
+        if( errors ) { renderErrors(response, errors); }
 
-      User.create(first_name, last_name, email, password, profile_picture).then(errors => {
-        if (errors) {
-          cloudinary.v2.uploader.destroy(public_id, { invalidate: true }, (error, result) => { 
-            console.log(result, error, "Profile Picture Deleted."); 
-          });
-          renderErrors(response, errors);
+        // Successful Sign Up
+        request.flash('success_msg', "Congrats! You're registered!");
+        response.redirect('/signup');
 
-        } else {
-          request.flash('success_msg', "Congrats! You're registered!");
-          response.redirect('/signup'); // TODO: Change this to whatever a user can access after login
-
-        }
-      }).catch(err => { console.log(err); });
-    }).catch(err => { console.log(err); });
+      }).catch(err => { renderErrors( response, err ); });
   }
 });
 
