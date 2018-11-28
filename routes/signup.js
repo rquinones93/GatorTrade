67d0958563a9e11bfd375e6bedb541a97131b07b
Robert <rquinones93@yahoo.com>
2018-11-28 00:31:47 -0800

    Signup Backend no longer required profile picture image.
    TODO: Update Profile Picture in User Dashboard

--- routes/signup.js
+++ routes/signup.js
@@ -1,40 +1,7 @@
 const express = require('express');
 const router = express.Router();
-let multer = require('multer');
-let cloudinary = require('cloudinary');
 const { User } = require('../database');
 const formValidation = require('../validation/signup_validation');
-
-// Set Up For Image Upload
-// Configure multer's diskStorage, when file is uploaded, create custom name
-let storage = multer.diskStorage({
-  filename: (request, file, callback) => {
-    callback(null, Date.now() + file.originalname);
-  }
-});
-
-// Any file must have a valid photo extension
-let imageFilter = (request, file, cb) => {
-  // accept image files only
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
-    return cb(new Error('Only image files are allowed!'), false);
-  }
-  cb(null, true);
-};
-
-// Create upload variable w/ multer object & configurations
-let upload = multer({
-  storage: storage,
-  fileFilter: imageFilter
-});
-
-// Set up cloudinary
-cloudinary.config({
-  cloud_name: 'hx8ztvtac',
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
 
 // Routes
 router.get('/', (request, response, next) => {
@@ -44,24 +11,25 @@ router.get('/', (request, response, next) => {
 });
 
 router.post('/', (request, response, next) => {
-
+  // Input Validation
   let formErrors = formValidation( request );
 
+  // Render Errors and Quit if Input Errors Found
   if( formErrors ) { 
     renderErrors(response, formErrors); 
 
   } else {
-
     const { first_name, last_name, email, password } = request.body;
 
-    // Create a new user
+    // Create a new user with form input
     User.create(first_name, last_name, email, password)
       .then( errors => {
+        // Render Errors if found ( email in use, invalid password )
         if( errors ) { renderErrors(response, errors); }
 
-        // Successful Sign Up
+        // Successful Sign Up - Redirect to login
         request.flash('success_msg', "Congrats! You're registered!");
-        response.redirect('/signup');
+        response.redirect('/login');
 
       }).catch(err => { renderErrors( response, err ); });
   }
