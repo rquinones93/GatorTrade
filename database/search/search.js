2e135dd280e276f023855f3b436515713d3383bd
Janda95 (Mars) <j.rettinghouse@gmail.com>
2018-12-01 15:37:54 -0800

    modified search with AND statement

--- database/search/search.js
+++ database/search/search.js
@@ -6,10 +6,10 @@ const db = require('../connection');
 const NO_SEARCH_INPUT_ALL = `SELECT * FROM items WHERE status = 'Approved';`;
 
 // Specific Category
-const NO_SEARCH_INPUT_CATEGORY = `SELECT * FROM items WHERE category, status = 'Approved' LIKE '%' || $2 || '%'`;
+const NO_SEARCH_INPUT_CATEGORY = `SELECT * FROM items WHERE status = 'Approved' AND category LIKE '%' || $2 || '%'`;
 
 // Specific Category with given search input
-const SEARCH_INPUT = `SELECT * FROM items WHERE category, status = 'Approved' LIKE '%' || $2 || '%' ` +
+const SEARCH_INPUT = `SELECT * FROM items WHERE status = 'Approved' AND category LIKE '%' || $2 || '%' ` +
                                          `AND (LOWER(title) LIKE  '%' || $1 || '%' ` +
                                          `OR LOWER(description) LIKE '%' || $1 || '%')`;
 
