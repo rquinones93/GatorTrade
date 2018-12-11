0d869cdcd8d675d715b131fbd687b344f1b883b0
Janda95 (Mars) <j.rettinghouse@gmail.com>
2018-12-10 19:30:17 -0800

    error messages now work

--- routes/resetpassword.js
+++ routes/resetpassword.js
@@ -13,20 +13,19 @@ router.post('/', (request, response, next) => {
   //get input from page
   let {newpassword, reenterpassword, secret, inputEmail, inputfirstname, inputlastname} = request.body;
   if (newpassword != reenterpassword) {
-    request.flash('error_msg', "Passwords do not match");
+    request.flash('error', "Passwords do not match!");
     response.redirect('/resetpassword');
     console.log("Passwords do not match");
 
   } else {
     User.recoverPassword(newpassword, inputEmail, inputfirstname, inputlastname)
       .then( (person) => {
-        console.log(person.length);
-        
+
         if (person.length == 1){ //user was changed
           request.flash('success_msg', "Password reset successful!");
           response.redirect(`/resetpassword`);
         } else if (person.length == 0) { //no user found with info
-          request.flash('error_msg', "Please Enter Valid User Info");
+          request.flash('error', "Please Enter Valid User Info!");
           response.redirect(`/resetpassword`);
         } else { //multiple users with same name, email, and last name
           request.flash('success_msg', "Password reset for multiple!");
