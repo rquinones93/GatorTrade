5b7c9b61732fd86e5d31a0b4f4603902e55e3e75
Robert <rquinones93@yahoo.com>
2018-12-18 16:58:01 -0800

    Updated user table to include public id column
    Updated seeder data to have all fields
    
    Route now removes existing photo from cloudinary and resets with new image

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../auth/actionAuthentication');
 let cloudinary = require('cloudinary');
+let multer = require('multer');
 const { Item, User } = require('../database');
 
 // Set up cloudinary
@@ -11,6 +12,31 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+// The following code is needed for image upload
+// Set Up For Image Upload
+// Configure multer's diskStorage, when file is uploaded, create custom name
+let storage = multer.diskStorage({
+  filename: (request, file, callback) => {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+
+// Any file must have a valid photo extension
+let imageFilter = (request, file, cb) => {
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
+};
+
+// Create upload variable w/ multer object & configurations
+let upload = multer({
+  storage: storage,
+  fileFilter: imageFilter
+});
+
+// Routes
 router.get('/', auth.userDashBoardAuthentication, (request, response, next) => {
   
   //need to check the user logged in for sellerID in the future
@@ -84,16 +110,39 @@ router.get('/remove_post/:post_id', auth.removePostAuthentication, (request, res
 // Upload and Update
 // Delete Old
 // Redirect to Settings Tab
-// router.post('/update_profile_picture', (request, response, next) => {
-//   let user_id = request.user.user_id;
+router.post('/update_profile_picture', upload.single('image'), (request, response, next) => {
+  const user_id = request.user.user_id;
+  const image_path = request.file.path;
 
-//   User.getUserDataById(user_id)
-//   .then((user) => {
-//     let old_profile_id = user.public_id
-//   }).catch();
-//   request.flash('success_msg', 'Profile Picture has been updated. View on Settings Tab.');
-//   response.redirect('/user');
-// });
+  User.getUserDataById(user_id)
+  .then((user) => {
+    // If old_profile_picture_id is empty assume seeder user or new user w/ default picture
+    let old_profile_picture_id = user.profile_picture_public_id;
+
+    // If the user has an old_profile_picture_id, their profile picture is already hosted
+    // on cloudinary and we need to delete it from our cloudinary account
+    // before uploading a new picture.
+    if(old_profile_picture_id != "") {
+      // Remove image with specified public id
+      cloudinary.uploader.destroy(old_profile_picture_id, function (error, result) {
+        console.log(result, error);
+      });
+    } 
+
+    // Upload new image
+    cloudinary.uploader.upload(image_path, (result) => {
+      const image_link = result.secure_url;
+      const public_id = result.public_id;
+
+      // Update user table DB with new profile picture values
+      User.updateProfilePicture(user.user_id, image_link, public_id)
+      .then(() => {
+        request.flash('success_msg', 'Profile Picture has been updated. View on Settings Tab.');
+        response.redirect('/user');
+      }).catch(err => console.log(err));
+    });
+  }).catch(err => console.log(err));
+});
 
 
 module.exports = router;
